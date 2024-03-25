3e6f07ce820a9eded038794a20b8f3b7ed3960fc
Carson McGuire <cmmcguir@ncsu.edu>
2024-03-25 16:29:03 -0400

    add v2 of buoy_FAM simulation; add libraries for modeling power dissipation mechanism downstream of FAM; add some Simscape testing models; misc associated changes

---
+++ buoy_sim_1D/Scripts/Sim_handling/run_buoy_FAM_sim_lab_scale.m
@@ -0,0 +1,106 @@
+%clear;clc;
+%Simulink.sdi.clear;
+
+CD = 0.5;
+buoy_density = 770;
+buoy_height = 0.1;
+buoy_radius = 0.75/2;
+spring_const = 97e3;
+damping = 300e3;
+
+qCol = 0;
+qRow = 0;
+powerMatrixMechanical = [];
+powerMatrixElectrical = [];
+wave_amplitude_sweep = 0.0582%(0.5:0.25:10)./2;
+%wave_period_sweep = 4.5:1:19.5;
+wave_period_sweep = 3%2.5:1:18.5;
+
+useParallel = false;
+
+%%% sequential
+if ~useParallel
+    for wave_amplitude = wave_amplitude_sweep
+        qRow = qRow + 1;
+        qCol = 0;
+        for wave_period = wave_period_sweep
+            qCol = qCol + 1;
+            tsc = sim("buoy_FAM_sim_v2.slx", "StopTime", "60");
+            %[powerMatrixMechanical, powerMatrixElectrical] = updatePowerMatrices(powerMatrixMechanical, powerMatrixElectrical, qRow, qCol, tsc);
+        end
+    end
+end
+
+%%% parallel
+if useParallel
+    q = 0;
+    for qRow = 1:length(wave_amplitude_sweep)
+        for qCol = 1:length(wave_period_sweep)
+            q = q+1;
+            in(q) = Simulink.SimulationInput('buoy_sim_v1');
+            in(q) = in(q).setVariable('wave_amplitude', wave_amplitude_sweep(qRow));
+            in(q) = in(q).setVariable('wave_period', wave_period_sweep(qCol));
+            in(q) = in(q).setVariable('spring_const', spring_const);
+            in(q) = in(q).setVariable('damping', damping);
+            in(q) = in(q).setVariable('CD', CD);
+            in(q) = in(q).setVariable('buoy_density', buoy_density);
+            in(q) = in(q).setVariable('buoy_radius', buoy_radius);
+            in(q) = in(q).setVariable('buoy_height', buoy_height);
+        end
+    end
+    out = parsim(in);
+    
+    q = 0;
+    for qRow = 1:length(wave_amplitude_sweep)
+        for qCol = 1:length(wave_period_sweep)
+            q = q+1;
+            tsc = out(q);
+            [powerMatrixMechanical, powerMatrixElectrical] = updatePowerMatrices(powerMatrixMechanical, powerMatrixElectrical, qRow, qCol, tsc);
+        end
+    end
+end
+%% plot
+try
+    figure(1)
+    clf
+    %levels = 0:0.2e7:2e7;
+    contourf(wave_period_sweep, wave_amplitude_sweep, powerMatrixMechanical)
+    colorbar()
+    xlabel('Wave period (s)')
+    ylabel('Wave amplitude (m)')
+    title(sprintf('Mechanical power matrix - K = %d N/m, C = %d N/(m/s)', spring_const, damping))
+    
+    figure(2)
+    clf
+    %levels = 0:2e4:5e6;
+    contourf(wave_period_sweep, wave_amplitude_sweep, powerMatrixElectrical)
+    colorbar()
+    xlabel('Wave period (s)')
+    ylabel('Wave amplitude (m)')
+    title(sprintf('Electrical power matrix - K = %d N/m, C = %d N/(m/s)', spring_const, damping))
+
+catch ME
+    % probably an scalar value for one or both of the wave parameters
+    figure(3)
+    hold on
+    plot(wave_period_sweep, powerMatrixElectrical./1000, 'DisplayName', sprintf('K = %d N/m', spring_const))
+    legend()
+    xlabel('Wave period (s)')
+    ylabel('Mean power generation (kW)')
+end
+
+
+%% functions
+function [powerMatrixMechanical, powerMatrixElectrical] = updatePowerMatrices(powerMatrixMechanical, powerMatrixElectrical, qRow, qCol, tsc)
+    %damper_power = get(tsc.logsout, 'damper_power');
+    %mean_damper_power = damper_power.Values.mean;
+    mechanical_energy = get(tsc.logsout, 'mechanical_energy');
+    mean_damper_power = mechanical_energy.Values.Data(end)/mechanical_energy.Values.Time(end); % time-averaged power (J/s)
+    powerMatrixMechanical(qRow, qCol) = mean_damper_power;
+
+    %electrical_power = get(tsc.logsout, 'electrical_power');
+    %mean_electrical_power = electrical_power.Values.mean;
+    electrical_energy = get(tsc.logsout, 'electrical_energy');
+    mean_electrical_power = electrical_energy.Values.Data(end)/electrical_energy.Values.Time(end); % time-averaged power (J/s)
+    powerMatrixElectrical(qRow, qCol) = mean_electrical_power;
+end
